ff95a08b6f24c76183a63c61d7bb5d228217b687
felipedrt <duarte_felipe022@hotmail.com>
2024-10-30 10:07:11 +0000

    feat([GIT-123]): correct message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'angular-husky' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-husky');
+    expect(app.title).toEqual('angular-husky-test');
   });
 
   it('should render title', () => {
